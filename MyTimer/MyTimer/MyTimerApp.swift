79c7f3ca3dd03922f1b1540a88a726bc5d4f3afe
hidepon503 <blue.passion625@gmail.com>
2023-12-29 00:48:44 +0900

    1st commit

---
+++ MyTimer/MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by 飯塚 秀幸 on 2023/12/29.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

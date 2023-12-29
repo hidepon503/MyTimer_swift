7b6526035b26d37da2ef6d6000a072c96f2ec309
hidepon503 <blue.passion625@gmail.com>
2023-12-29 18:42:29 +0900

    完成

---
+++ MyTimer/MyTimer/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  MyTimer
+//
+//  Created by 飯塚 秀幸 on 2023/12/29.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
